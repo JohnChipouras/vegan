d42030248340b4e28a321308330009153f70f483
John Chipouras <chipouras@gmail.com>
2015-02-24 17:10:08 -0500

    liking how its coming together

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Vegan Grocery List"
+    assert_select "title", "Vegan Grocery List"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Vegan Grocery List"
+    assert_select "title", "Vegan Grocery List"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Vegan Grocery List"
+    assert_select "title", "Vegan Grocery List"
   end
 
 end
