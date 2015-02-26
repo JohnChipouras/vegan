c37d135de08965f2b16b2fde5e9e591640a123a5
John Chipouras <chipouras@gmail.com>
2015-02-25 20:17:52 -0500

    new test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,12 +12,6 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "Vegan Grocery List"
   end
-
-  test "should get about" do
-    get :about
-    assert_response :success
-    assert_select "title", "Vegan Grocery List"
-  end
   
   test "should get contact" do
     get :contact
