4a3ce7e8f8934d633da1e68cfc451b5db11ae93f
John Chipouras <chipouras@gmail.com>
2015-03-04 12:25:06 -0500

    added email for pw reset but need to review test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,11 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "A welcome gift! From Vegan Grocery List"
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Mmmm new password"
   end
 end
