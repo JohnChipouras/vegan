5d27ea5f51820e12969da5150ca821324958300d
John Chipouras <chipouras@gmail.com>
2015-04-07 10:55:19 -0400

    paginate error addressed, maybe a mistake taking activated: FILL IN out of def index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   
   
   def index
-    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
   end
   
   def show
