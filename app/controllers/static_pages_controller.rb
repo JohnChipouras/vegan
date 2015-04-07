35321fba77a11d345d3b7b0aa7b1ed4df5d5d8b6
John Chipouras <chipouras@gmail.com>
2015-04-07 12:21:11 -0400

    feed error attempted correction again

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,10 @@
 class StaticPagesController < ApplicationController
   
   def home
-    @list = current_user.list.build if logged_in?
-    @feed_items = current_user.feed.paginate(page: params[:page])
+    if logged_in?
+      @list = current_user.list.build 
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def about
