dd0544818d366caee52fc6dc7f620df206946fd5
John Chipouras <chipouras@gmail.com>
2015-04-08 13:35:59 -0400

    Add user following

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   
   def home
     if logged_in?
-      @list = current_user.list.build 
+      @list  = current_user.list.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
